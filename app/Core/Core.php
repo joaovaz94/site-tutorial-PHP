4d3e26f54f656b26be1c75a154ec826490d76bbf
joaovaz94 <joaovaz94.4@gmail.com>
2021-01-20 16:25:48 -0300

    Finalizada a view de postagem, modulo 7

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -15,6 +15,11 @@
 		$controller = 'ErrorController';
 	    }
 
-	    call_user_func_array(array(new $controller, $acao), array());
+	    if(isset($urlGet['id']) && $urlGet['id'] != null) {
+		$id = $urlGet['id'];
+	    } else {
+		$id = null;
+	    }
+	    call_user_func_array(array(new $controller, $acao), array('id' => $id));
 	}
     }
